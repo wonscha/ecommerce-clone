a75c1eb379870a51c16ff5e8106c330cf9846f87
Wonsuk Cha <wonscha@outlook.com>
2021-09-11 20:10:39 -0400

    User registration & password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,4 +24,51 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser }
+// @desc    Auth user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json(req.user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+// @desc    Register a new user
+// @route   GET /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile, registerUser }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,14 @@ userSchema.methods.matchPassword = async function (entenredPassword) {
   return await bcrypt.compare(entenredPassword, this.password)
 }
 
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+  }
+  next()
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
